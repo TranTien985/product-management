233c7270c607c47cd30f53e4ce31f429ddb13161
Tiennk5 <154210654+Tiennk5@users.noreply.github.com>
2025-06-09 09:40:37 +0700

    update thông báo

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -153,14 +153,16 @@ if(formChangeMulti){
 //Show alert
 const showAlert = document.querySelector("[show-alert]");
 if (showAlert) {
-  const time = showAlert.getAttribute("data-time");
+  const time = parseInt(showAlert.getAttribute('data-time'));
+  const buttonClose = document.querySelector("[close-alert]");
 
+  setTimeout(() => {
+    showAlert.classList.add("alert-hidden");
+  }, time);
 
-  if (!isNaN(time)) {
-    setTimeout(() => {
-      showAlert.classList.add("alert-hidden");
-    }, time);
-  }
+  buttonClose.addEventListener("click", () => {
+    showAlert.classList.add("alert-hidden")
+  });
 }
 
 //End Show alert
