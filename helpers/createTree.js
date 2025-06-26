77f006d483cd0fa6e4ff31569c9754e47eb1ee33
Tiennk5 <154210654+Tiennk5@users.noreply.github.com>
2025-06-26 15:14:47 +0700

    update stt

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,3 +1,4 @@
+let count = 0
 // phải tạo ra một hàm riêng để chạy trước tại khi mình export
 // đoạn code này sẽ được thực hiện bên file khác nên nó sẽ xảy ra lỗi ko tìm thấy hàm đệ quy
 //  nên ta phải tạo 1 hàm chạy đệ quy riêng và 1 hàm export riêng
@@ -5,7 +6,9 @@ const createTree = (arr, parentId ="") => {
   const tree = [];
   arr.forEach((item) => {
     if(item.parent_id === parentId){
+      count++
       const newItem = item 
+      newItem.index = count
       const children = createTree(arr, item.id);        
       if(children.length > 0){
         newItem.children = children
@@ -18,6 +21,8 @@ const createTree = (arr, parentId ="") => {
 // muốn xem dữ liệu thì phải mở ở mục console trên devtools của Nodejs
 
 module.exports.tree = (arr, parentId = "") => {
+  count = 0 // tại biến count là biến server nên khi mà load lại trang 
+  // thì biến count vẫn giữ nguyên nên ta phải set nó về 0 trước khi sử dụng hàm đệ quy
   const tree = createTree(arr, parentId = "");
   return tree;  
 }
