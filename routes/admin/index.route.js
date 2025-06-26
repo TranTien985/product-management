abd1da72b368d940b7921e9b1f7e5cecd26e3129
Tiennk5 <154210654+Tiennk5@users.noreply.github.com>
2025-06-26 08:47:32 +0700

    danh mục sản phẩm (phần 1)

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,7 +1,8 @@
 // muốn import file routes vào file index thì phải export nó ra trước
+const systemConfig = require("../../config/system")
 const dashboardRouter = require("./dashboard.route")
 const productRouter = require("./product.routes")
-const systemConfig = require("../../config/system")
+const productCategoryRouter = require("./products-category.route")
 
 module.exports = (app) =>{
   const PATH_ADMIN = systemConfig.prefixAdmin;
@@ -9,5 +10,7 @@ module.exports = (app) =>{
   app.use(PATH_ADMIN + '/dashboard', dashboardRouter);
   
   app.use(PATH_ADMIN + '/products', productRouter);
+
+  app.use(PATH_ADMIN + '/products-category', productCategoryRouter);
 
 }
