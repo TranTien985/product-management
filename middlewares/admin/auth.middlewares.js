e1a3e4617343ae2ae6661299589cfe032ef7176b
Tiennk5 <154210654+Tiennk5@users.noreply.github.com>
2025-07-16 20:41:21 +0700

    Thêm authorization

--- middlewares/admin/auth.middlewares.js
+++ middlewares/admin/auth.middlewares.js
@@ -1,15 +1,22 @@
 const systemConfig = require("../../config/system");
 const Account = require("../../models/account.model")
+const Role = require("../../models/roles.model")
 
-// 
+// check token khi đăng nhập
 module.exports.requireAuth = async (req, res, next) => {
   if(!req.cookies.token){
     res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
   }else{
-    const user = await Account.findOne({ token: req.cookies.token })
+    const user = await Account.findOne({ token: req.cookies.token }).select("-password");
     if(!user){
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     }else{
+      const role = await Role.findOne({
+        _id: user.role_id
+      }).select("title permissions");
+      // tất cả file pug đều có thể sử dụng biến này
+      res.locals.user = user 
+      res.locals.role = role 
       next();
     }
   }
