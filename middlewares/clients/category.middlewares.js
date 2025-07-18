49bd8b84857c046b8b42895c1c2edaaca3c3335b
Tiennk5 <154210654+Tiennk5@users.noreply.github.com>
2025-07-18 09:44:19 +0700

    tạo menu cho trang client

--- middlewares/clients/category.middlewares.js
+++ middlewares/clients/category.middlewares.js
@@ -1,9 +1,7 @@
 const ProductCategory = require("../../models/product-category.model"); //database
-const systemConfig = require("../../config/system");
 const createTreeHelper = require("../../helpers/createTree")
 
-// [GET] /
-module.exports.index = async (req, res) => {
+module.exports.category = async (req, res, next) => {
   let find = {
     deleted: false,
   };
@@ -11,9 +9,9 @@ module.exports.index = async (req, res) => {
   const productCategory = await ProductCategory.find(find);
 
   const newProductCategory = createTreeHelper.tree(productCategory);
-  // vì biến này cần dùng lại nhiều nên ta phải tạo middlewares để tái sử dụng nó
 
-  res.render("client/pages/home/index", {
-    pageTitle: "Trang chủ",
-  });
-};
+  // vì biến này cần dùng lại nhiều nên ta phải tạo middlewares để tái sử dụng nó
+  res.locals.layoutProductCategory = newProductCategory;
+
+  next()
+}
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,10 +1,14 @@
+const categoryMiddlewares = require("../../middlewares/clients/category.middlewares");
+
 // muốn import file routes vào file index thì phải export nó ra trước
-const homeRouter = require("./home.route")
-const productRouter = require("./products.routes")
+const homeRouter = require("./home.route");
+const productRouter = require("./products.routes");
 
+module.exports = (app) => {
+  app.use(categoryMiddlewares.category)// khi bất kì vào trang nào thì nó đều đi qua cái thằng này
 
-module.exports = (app) =>{
-    // trong file home cả product đã có get rồi nên ở đây chỉ cần use là dc 
-    app.use('/', homeRouter);
-    app.use('/products', productRouter);
-}
+  // trong file home cả product đã có get rồi nên ở đây chỉ cần use là dc
+  app.use("/",  homeRouter);
+
+  app.use("/products",  productRouter);
+};
