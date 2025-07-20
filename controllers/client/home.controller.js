d78580f7c5a2840df314acbd31a9e2404bb1526b
Tiennk5 <154210654+Tiennk5@users.noreply.github.com>
2025-07-20 20:06:18 +0700

    hiển thị danh sách sản phẩm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -10,11 +10,21 @@ module.exports.index = async (req, res) => {
     deleted: false,
     availabilityStatus: "In Stock"
   }).limit(3);
+  const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured)
+  // end lấy ra sản phẩm nổi bật 
 
-  const newProducts = productsHelper.priceNewProducts(productsFeatured)
+  // Lấy ra sản phẩm mới nhất 
+  const productsNew = await Product.find({
+    deleted: false,
+    availabilityStatus: "In Stock"
+  }).sort({ position : "desc"}).limit(6);
+
+  const newProductsNew = productsHelper.priceNewProducts(productsNew)
+  // end Lấy ra sản phẩm mới nhất 
 
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    productsFeatured: newProducts
+    productsFeatured: newProductsFeatured,
+    productsNew: newProductsNew
   });
 };
