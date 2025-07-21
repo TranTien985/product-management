991b70042d31c3ce5ec8566607d24f370eb2b8cc
Tiennk5 <154210654+Tiennk5@users.noreply.github.com>
2025-07-21 16:15:22 +0700

    làm trang danh sách sản phẩm bên client

--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,5 +1,8 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
+
 const productsHelper = require("../../helpers/products");
+const productCategoryHelper = require("../../helpers/product-category")
 
 // [GET] /products
 module.exports.index = async (req, res) => {
@@ -37,4 +40,34 @@ module.exports.detail = async (req, res) => {
   } catch (error) {
     res.redirect(`/products`);
   }
+}
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+
+  const category = await ProductCategory.findOne({
+    slug: req.params.slugCategory,
+    availabilityStatus: "In Stock",
+    deleted: false,
+  });
+
+  // vì hàm trên dùng async-await nên dưới đây cũng phải dùng await để chờ lấy ra hết data
+  const listSubCategory = await productCategoryHelper.getSubCategory(category.id);
+  
+  const listSubCategoryId = listSubCategory.map(item => item.id);
+  
+
+  const products = await Product.find({
+    // lấy tất phần tử khi ta bấm vào danh mục cha (phải có tất cả sản phẩm ở danh mục con)
+    product_category_id: { $in: [category.id, ...listSubCategoryId] },
+    deleted: false
+  }).sort({ position : "desc" });
+  
+  
+  const newProducts = productsHelper.priceNewProducts(products)
+
+  res.render("client/pages/products/index", {
+    pageTitle: category.title,
+    products: newProducts
+  });
 }
