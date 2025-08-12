269fc00d474bffb3af7332927792b44be25569ea
Tiennk5 <154210654+Tiennk5@users.noreply.github.com>
2025-08-12 09:50:21 +0700

    tính năng đăng nhập bên client

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,10 +1,10 @@
 const User = require("../../models/user.model")
 const md5 = require("md5")
 
-// [POST] /user/register
+// [GET] /user/register
 module.exports.register = async (req, res) => {
   res.render("client/pages/user/register", {
-    pageTitle: 'Kết quả tìm kiếm',
+    pageTitle: 'Đăng kí tài khoản',
   });
 }
 
@@ -25,8 +25,46 @@ module.exports.registerPost = async (req, res) => {
 
   res.cookie("tokenUser", user.tokenUser)
   res.redirect("/"); 
+}
 
-  console.log(user);
+// [POST] /user/login
+module.exports.login = async (req, res) => {
+  res.render("client/pages/user/login", {
+    pageTitle: 'Đăng nhập tài khoản',
+  });
+}
+
+// [POST] /user/loginPost
+module.exports.loginPost = async (req, res) => {
+  const email = req.body.email 
+  const password = req.body.password 
   
-  res.send("ok")
+  const user = await User.findOne({
+    email : email,
+    deleted: false, 
+  });
+
+  // check xem có tồn tại tài khoản này không
+  if(!user){
+    req.flash("error", "Email không tồn tại");
+    res.redirect(req.get("Referer") || "/");
+    return;
+  }
+
+  // check password 
+  if(md5(password) !== user.password){
+    req.flash("error", "Sai mật khẩu");
+    res.redirect(req.get("Referer") || "/");
+    return;
+  }
+
+  // check trạng thái của tài khoản
+  if(user.status == "inactive"){
+    req.flash("error", "Tài khoản đang bị khóa");
+    res.redirect(req.get("Referer") || "/");
+    return;
+  }
+
+  res.cookie("tokenUser", user.tokenUser);
+  res.redirect("/") 
 }
